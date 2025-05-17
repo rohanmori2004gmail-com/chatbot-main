ab80b3115a52a2eedf2e0f8a7874fa2df47e1a5f
rohanmori2004gmail-com <rohanmori2004@gmail.com>
2025-05-17 17:27:40 +0530

    Add files via upload

--- chatbot-main/app.py
+++ chatbot-main/app.py
@@ -1,1 +1,23 @@
 #chatbot-main
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
+from chat import get_response
+
+app = Flask(_name_)
+CORS(app)
+
+
+app.get("/")
+def index_get():
+   return render_template("base.html")
+
+@app.post("/predict")
+def predict():
+    text = request.get_json().get("message")
+    #TODO: check if text is valid
+    response = get_response (text)
+    message = {"answer": response}
+    return jsonify (message)
+
+if _name_ == "_main_":
+    app.run(debug=True)
